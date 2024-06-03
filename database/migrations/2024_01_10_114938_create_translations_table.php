aa6e95741b43a67de6b8a455d490ce069519e074
Astatroth <astatroth@gmail.com>
2024-06-03 17:16:18 +0500

    Initial

---
+++ database/migrations/2024_01_10_114938_create_translations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->collation = 'utf8mb4_bin';
+            $table->id();
+            $table->integer('status')->default(0);
+            $table->string('locale');
+            $table->string('group');
+            $table->text('key');
+            $table->text('value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('translations');
+    }
+};
