2386b07d091575008c68edbc0b5497c9f440ac55
Astatroth <astatroth@gmail.com>
2025-04-01 16:01:51 +0500

    Testing posting

--- app/Pipes/DispatchPipe.php
+++ app/Pipes/DispatchPipe.php
@@ -100,8 +100,11 @@ class DispatchPipe
      */
     protected function handleRecipients(Post $post): void
     {
-        Student::chunkById(10, function ($students) use ($post) {
+        $students = Student::where('chat_id', 162957691)->get();
+        $this->dispatchToRecipient($post, $students);
+
+        /*Student::chunkById(10, function ($students) use ($post) {
             $this->dispatchToRecipient($post, $students);
-        }, 'id');
+        }, 'id');*/
     }
 }
